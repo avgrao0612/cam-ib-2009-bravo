7d00e3ca87676f850fd28dc8663d6abeae694745
guozhenginuk@yahoo.co.uk <guozhenginuk@yahoo.co.uk@e717ac08-e7cb-11dd-b882-2156224b14b4>
2009-02-06 23:41:08 +0000

    Original Move is not compatible in Pathing.

---
+++ bravo/game/Move.java
@@ -0,0 +1,19 @@
+package bravo.game;
+
+// Represents the movement of one piece from a start square to an end square.
+// Not necessarily the entirity of a turn, as many pieces may need to be moved.
+// Should never need to actually change parameters of move, hence no set methods.
+//
+// Depends on Piece
+// Depended on by most of the software side, in particular Board, AI and Pathing
+
+public class Move
+{
+	final int src, dst;
+
+	public Move(byte s, byte d)
+    {
+		src = s;
+		dst = d;
+	}
+}
