da31b6e6f41af63d5f66c4bf6d4e64e53811cd56
infinity0x <infinity0x@e717ac08-e7cb-11dd-b882-2156224b14b4>
2009-02-07 05:33:40 +0000

    fix Move back to using bytes and fix coord methods in Pathing

--- bravo/io/Pathing.java
+++ bravo/io/Pathing.java
@@ -5,7 +5,7 @@ package bravo.game;
 
 import java.util.Vector;
 class GoToTop extends Thread  //The thread responsible for calculating the path towards the top
-{    
+{
      private int[][] board;
      private int x;
      private int y;
@@ -14,7 +14,7 @@ class GoToTop extends Thread  //The thread responsible for calculating the path 
      private Vector paths;
 
      public GoToTop(int[][] board,int x,int y,int topBound,long path,Vector paths)
-     {    
+     {
           this.board=board;
           this.x=x;
           this.y=y;
@@ -24,7 +24,7 @@ class GoToTop extends Thread  //The thread responsible for calculating the path 
      }
 
      private int isEndReached()
-     {    
+     {
           if(x>0&&y>0&&board[x-1][y-1]==3) return 1;
           else if(x>0&&board[x-1][y]==3) return 2;
           else if(x>0&&y<board.length-1&&board[x-1][y+1]==3) return 3;
@@ -45,22 +45,22 @@ class GoToTop extends Thread  //The thread responsible for calculating the path 
          7 | 6 | 5
            |   |
            BOTTOM
-*/     
-     
+*/
+
      public void run()
-     {    
+     {
          int isPathComplete=isEndReached();
          if(isPathComplete!=0)
-         {  
+         {
              path*=10;
              path+=isPathComplete;
              paths.addElement(path);
          }
-/*If the distination can be reached at the next move, store this move and add this 
+/*If the distination can be reached at the next move, store this move and add this
   path to the set of all valid paths.
   The path is coded as a long, each digit represent a move and the whole sequence is
   in the reverse order.
-*/        
+*/
          else if(x-1<=topBound) return;
 //If the destination cannot be reached at the next move, yet it is about to pass the
 //destination square, terminate the thread.
@@ -75,14 +75,14 @@ class GoToTop extends Thread  //The thread responsible for calculating the path 
               {
 		           g1=new GoToTop(board,x-1,y,topBound,path*10+2,paths);
                    g1.start();  //Recursion towards top
-              } 
+              }
               if(x-1>=0&&y-1>=0&&board[x-1][y-1]==0)
-              {   
+              {
 		           g2=new GoToTop(board,x-1,y-1,topBound,path*10+1,paths);
                    g2.start();  //Recursion towards topleft
               }
               if(x-1>=0&&y+1<board[x-1].length&&board[x-1][y+1]==0)
-              {   
+              {
 		           g3=new GoToTop(board,x-1,y+1,topBound,path*10+3,paths);
                    g3.start();  //Recursion towards topright
               }
@@ -97,7 +97,7 @@ class GoToTop extends Thread  //The thread responsible for calculating the path 
                    g5.start();  //Recursion towards right
               }
               try
-              {   
+              {
                   if(g1!=null) g1.join();
                   if(g2!=null) g2.join();
                   if(g3!=null) g3.join();
@@ -110,7 +110,7 @@ class GoToTop extends Thread  //The thread responsible for calculating the path 
 }
 
 class GoToRight extends Thread  //The thread responsible for calculating the path towards the right
-{    
+{
      private int[][] board;
      private int x;
      private int y;
@@ -119,7 +119,7 @@ class GoToRight extends Thread  //The thread responsible for calculating the pat
      private Vector paths;
 
      public GoToRight(int[][] board,int x,int y,int rightBound,long path,Vector paths)
-     {    
+     {
           this.board=board;
           this.x=x;
           this.y=y;
@@ -127,9 +127,9 @@ class GoToRight extends Thread  //The thread responsible for calculating the pat
           this.path=path;
           this.paths=paths;
      }
- 
+
      private int isEndReached()
-     {    
+     {
           if(x>0&&y>0&&board[x-1][y-1]==3) return 1;
           else if(x>0&&board[x-1][y]==3) return 2;
           else if(x>0&&y<board.length-1&&board[x-1][y+1]==3) return 3;
@@ -142,7 +142,7 @@ class GoToRight extends Thread  //The thread responsible for calculating the pat
      }
 
      public void run()
-     {    
+     {
           int isPathComplete=isEndReached();
           if(isPathComplete!=0)
           {
@@ -159,17 +159,17 @@ class GoToRight extends Thread  //The thread responsible for calculating the pat
               GoToRight g5=null;
 
               if(y+1<board[x].length&&board[x][y+1]==0)
-              {   
+              {
 		           g1=new GoToRight(board,x,y+1,rightBound,path*10+4,paths);
                    g1.start();  //Recursion towards right
               }
               if(x-1>=0&&y+1<board[x-1].length&&board[x-1][y+1]==0)
-              {   
+              {
 		           g2=new GoToRight(board,x-1,y+1,rightBound,path*10+3,paths);
                    g2.start();  //Recursion towards topright
               }
               if(x+1<board.length&&y+1<board[x+1].length&&board[x+1][y+1]==0)
-              {   
+              {
 		           g3=new GoToRight(board,x+1,y+1,rightBound,path*10+5,paths);
                    g3.start();  //Recursion towards bottomright
               }
@@ -197,7 +197,7 @@ class GoToRight extends Thread  //The thread responsible for calculating the pat
 }
 
 class GoToBottom extends Thread  //Thread responsible for calculating the path towards the bottom
-{   
+{
      private int[][] board;
      private int x;
      private int y;
@@ -206,7 +206,7 @@ class GoToBottom extends Thread  //Thread responsible for calculating the path t
      private Vector paths;
 
      public GoToBottom(int[][] board,int x,int y,int bottomBound,long path,Vector paths)
-     {    
+     {
           this.board=board;
           this.x=x;
           this.y=y;
@@ -243,17 +243,17 @@ class GoToBottom extends Thread  //Thread responsible for calculating the path t
                GoToBottom g5=null;
 
                if(x+1<board.length&&board[x+1][y]==0)
-               {   
+               {
                    g1=new GoToBottom(board,x+1,y,bottomBound,path*10+6,paths);
                    g1.start();  //Recursion towards the bottom
                }
                if(x+1<board.length&&y+1<board[x+1].length&&board[x+1][y+1]==0)
-               {   
+               {
                    g2=new GoToBottom(board,x+1,y+1,bottomBound,path*10+5,paths);
                    g2.start();  //Recursion towards the bottomright
                }
                if(x+1<board.length&&y-1>=0&&board[x+1][y-1]==0)
-               {   
+               {
                    g3=new GoToBottom(board,x+1,y-1,bottomBound,path*10+7,paths);
                    g3.start();  //Recursion towards the bottomleft
                }
@@ -283,7 +283,7 @@ class GoToBottom extends Thread  //Thread responsible for calculating the path t
 }
 
 class GoToLeft extends Thread  //Thread responsible for calculating the path towards the left
-{    
+{
      private int[][] board;
      private int x;
      private int y;
@@ -300,7 +300,7 @@ class GoToLeft extends Thread  //Thread responsible for calculating the path tow
           this.paths=paths;
      }
      private int isEndReached()
-     {    
+     {
           if(x>0&&y>0&&board[x-1][y-1]==3) return 1;
           else if(x>0&&board[x-1][y]==3) return 2;
           else if(x>0&&y<board.length-1&&board[x-1][y+1]==3) return 3;
@@ -313,34 +313,34 @@ class GoToLeft extends Thread  //Thread responsible for calculating the path tow
      }
 
      public void run()
-     {    
+     {
           int isPathComplete=isEndReached();
           if(isPathComplete!=0)
-          { 
+          {
               path*=10;
               path+=isPathComplete;
               paths.addElement(path);
           }
           else if(y-1<=leftBound) return;
           else
-          {  
+          {
               GoToLeft g1=null;
               GoToLeft g2=null;
               GoToLeft g3=null;
               GoToLeft g4=null;
               GoToLeft g5=null;
               if(y-1>=0&&board[x][y-1]==0)
-              {   
+              {
                   g1=new GoToLeft(board,x,y-1,leftBound,path*10+8,paths);
                   g1.start();  //Recursion towards the right
               }
               if(x+1<board.length&&y-1>=0&&board[x+1][y-1]==0)
-              {  
+              {
                   g2=new GoToLeft(board,x+1,y-1,leftBound,path*10+7,paths);
                   g2.start();  //Recursion towards the bottomright
               }
               if(x-1>=0&&y-1>=0&&board[x-1][y-1]==0)
-              {   
+              {
                   g3=new GoToLeft(board,x-1,y-1,leftBound,path*10+1,paths);
                   g3.start();  //Recursion towards the topright
               }
@@ -368,13 +368,13 @@ class GoToLeft extends Thread  //Thread responsible for calculating the path tow
      }
 }
 
-public class Pathing 
+public class Pathing
 {
 /*Coordinate system of boardStatus:
                    White Player
-       0   1   2   3   4   5   6   7   8   9  y    
+       0   1   2   3   4   5   6   7   8   9  y
       _______________________________________
-  0  |___|___|___|___|___|___|___|___|___|___|    
+  0  |___|___|___|___|___|___|___|___|___|___|
   1  |___|_W_|_B_|_W_|_B_|_W_|_B_|_W_|_B_|___|
   2  |___|_B_|_W_|_B_|_W_|_B_|_W_|_B_|_W_|___|
   3  |___|_W_|_B_|_W_|_B_|_W_|_B_|_W_|_B_|___|
@@ -387,19 +387,7 @@ public class Pathing
   x                Black Player
 For each element, 0 represents an empty square, 2 represents an occupied square and 3
 represents the distination square of a move.
-*/    
-    private int[][] squareNumber={{0x98,0x90,0x91,0x92,0x93,0x94,0x95,0x96,0x97,0x99},
-                                   {0x78,0x70,0x71,0x72,0x73,0x74,0x75,0x76,0x77,0x79},
-                                   {0x68,0x60,0x61,0x62,0x63,0x64,0x65,0x66,0x67,0x69},
-                                   {0x58,0x50,0x51,0x52,0x53,0x54,0x55,0x56,0x57,0x59},
-                                   {0x48,0x40,0x41,0x42,0x43,0x44,0x45,0x46,0x47,0x49},
-                                   {0x38,0x30,0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x39},
-                                   {0x28,0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27,0x29},
-                                   {0x18,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x19},
-                                   {0x08,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x09},
-                                   {0x88,0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x89}};
-//Reference matrix used to convert the identifier of each square into coordinates of
-//boardstatus and vise varsa.
+*/
 
     public int[] path (Move move)
     {
@@ -448,28 +436,25 @@ represents the distination square of a move.
 //individual movement to achieve it. It returns null if there is no valid
 //path that can be generated, though theoretically that should never happen
 //as long as the game is played in a resonable manner.
-    
-    private int xcoordinate(int a)
-    {    
-        for(int i=0;i<squareNumber.length;i++)
-            for(int j=0;j<squareNumber[i].length;j++)
-                if(squareNumber[i][j]==a) return i;
-        return -1;
+
+    private int xcoordinate(byte a)
+    {
+        int x = a & 0x0F;
+        return (x < 8)? x+1: (x == 8)? 0: (x == 9)? 9: -1;
     }
 //Find the x-coordinate of a square given its square number. Return -1
 //if no such square exists.
 
-    private int ycoordinate(int a)
-    {    for(int i=0;i<squareNumber.length;i++)
-             for(int j=0;j<squareNumber[i].length;j++)
-                  if(squareNumber[i][j]==a) return j;
-         return -1;
+    private int ycoordinate(byte a)
+    {
+        int y = a >>> 4;
+        return (y < 8)? y+1: (y == 8)? 0: (y == 9)? 9: -1;
     }
 //Find the y-coordinate of a square given its square number. Return -1
 //if no such square exists.
 
     private int[][] setBoard(Move move)
-    {  
+    {
         int[][] board=new int[10][];
         for(int i=0;i<board.length;i++)
            board[i]=new int[10];
@@ -489,7 +474,7 @@ represents the distination square of a move.
         int endX=xcoordinate(move.dst);
         int endY=ycoordinate(move.dst);
         if(endY==startY)
-        {   
+        {
             if(endX<startX) return 1;
             else return 3;
         }
@@ -521,7 +506,7 @@ represents the distination square of a move.
     {    long n=l.longValue();
          int i=0;
          while(n>0)
-         {    
+         {
               i++;
               n/=10;
          }
@@ -530,7 +515,7 @@ represents the distination square of a move.
 //Count the number of movements needed for a calculated path.
 
     private int[] bestRoute(Vector v)
-    {   
+    {
          if(v.size()==0) return null;
          else
          {
@@ -552,7 +537,7 @@ represents the distination square of a move.
              }
              return c;
          }
-    }    
+    }
 //Find the shortest path among all paths. Return null if no valid paths available
     public static void main(String[] args)
     {
