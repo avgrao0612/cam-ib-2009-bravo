d8d8fa02a2f44a79d5c0bb398548bbc7697f84ad
guozhenginuk@yahoo.co.uk <guozhenginuk@yahoo.co.uk@e717ac08-e7cb-11dd-b882-2156224b14b4>
2009-02-20 15:32:56 +0000

    Basic structure of HWInterface, still incomplete. Some methods are about to be deleted and some are to merged.

--- trunk/bravo/io/HWInterface.java
+++ trunk/bravo/io/HWInterface.java
@@ -1,48 +1,254 @@
-// Part of the Software-Hardware Interface, this class has methods which communicate with the hardware
-// Will most likely use Java libraries for accessing the serial port
+//The Java side of the software-hardware interface. This class is responsible for various
+//signal transmission and receival through the serial cable between the host computer and the
+//DE2 board.
+//However, this class is very machine-dependent, since it requires a special java extension
+//package called Java Communication API, which is not included in the standard JDK. So only the
+//machines with this package can run this code.
+
+package bravo.io;
+
+import javax.comm.*;
+import java.io.*;
+
+public class HWInterface
+{
+     private final byte[] DIRECTION={0x38,0x00,0x08,0x10,0x18,0x20,0x28,0x30};
+     private final byte SCANREQUEST=(byte)0x80,MAGNETON=0x60,MAGNETOFF=0x40,VALIDBOARD=0x60;
+     private final byte ACKNOWLEDGEMENT=0x40,ENDOFPLAYER=0x00,
+                        RESIGN=0x20,ERRORSIGNAL=(byte)0xff;
+
+     private InputStream is;
+     private OutputStream os;
+
+     public HWInterface(String portName)
+     {
+         try
+         {
+             CommPortIdentifier cpi=CommPortIdentifier.getPortIdentifier(portName);
+             SerialPort port=(SerialPort)cpi.open(this.getClass().getName(),1000);
+             port.setSerialPortParams(200000,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+             is=port.getInputStream();
+             os=port.getOutputStream();
+         }
+         catch(Exception e)
+         {
+             System.out.print("Initialisation error has occured: ");
+             System.err.println(e);
+         }
+     }
+//The name of the serial port needs to be identified before run this code.
+
+     public void moveHead(int direction)
+     {
+         byte[] data=new byte[1];
+         try
+         {
+             data[0]=DIRECTION[direction-1];
+             os.write(data);
+         }
+         catch(IOException e)
+         {
+             System.out.print("Moving error has occured: ");
+             System.err.println(e);
+         }
+         try
+         {
+             byte acknowledgement;
+             while(true)
+             {
+                 acknowledgement=(byte)is.read(data);
+                 if(acknowledgement==ACKNOWLEDGEMENT) break;
+//Any other signals received will be discarded apart from ACKNOWLEDGEMENT.
+             }
+
+         }
+         catch(Exception e)
+         {
+             System.out.print("Acknowledgement receiving error has occured: ");
+             System.err.println(e);
+         }
+     }
 //
-// Depended on by most of the project, in particular Sensor and Pathing
-// Depends on the chosen library and the communications link, as well as the corresponding Verilog module
-package bravo.game;
-public abstract class HWInterface {
 
-public final int NORTHWEST=1,NORTH=2,NORTHEAST=3,EAST=4,SOUTHEAST=5,SOUTH=6,SOUTHWEST=7,WEST=8,CENTRE=0;
-//Based on the convention used in Pathing
+     public void magnetOn()
+     {
+         try
+         {
+             byte[] data={MAGNETON};
+             os.write(data);
+         }
+         catch(IOException e)
+         {
+             System.out.print("Magnet powering up error has occured: ");
+             System.err.println(e);
+         }
+     }
+// Turn the electromagnet on
 
-// Constructor should not need to take any arguments
+     public void magnetOff()
+     {
+         try
+         {
+             byte[] data={MAGNETOFF};
+             os.write(data);
+         }
+         catch(IOException e)
+         {
+             System.out.print("Magnet powering down error has occured: ");
+             System.err.println(e);
+         }
+     }
+// Turn the electromagnet off
 
-// Moves head in given direction.  Use constants as described above.
-// Returns true when completed
-public abstract boolean moveHead (int direction);
+     public boolean[] scan()
+     {
+         try
+         {
+             byte[] data={SCANREQUEST};
+             os.write(data);
+         }
+         catch(IOException e)
+         {
+             System.out.print("Scanning error has occured: ");
+             System.err.println(e);
+         }
+         byte[] rowState=new byte[20];
+         int successful=-1;
+         try
+         {
+             successful=is.read(rowState);
+             if(successful<0) return null;
+         }
+         catch(Exception e)
+         {
+             System.out.print("Board state receiving error has occured: ");
+             System.err.println(e);
+         }
+         int[][] board=new int[10][10];
+         for(int i=0;i<rowState.length;i++)
+         {
+             int rowNumber=i/2;
+             byte rowAlinement=rowState[i];
+             if(i%2==1)
+             {
+                 for(int j=4;j>=0;j--)
+                 {
+                     board[rowNumber][j]=rowAlinement%2;
+                     rowAlinement/=2;
+                 }
 
-// Turns the electromagnet to the On setting
-// Returns true when completed
-public abstract boolean magnetOn ();
+             }
+             else
+             {
+                 for(int j=9;j>=5;j--)
+                 {
+                     board[rowNumber][j]=rowAlinement%2;
+                     rowAlinement/=2;
+                 }
+             }
+         }
 
-// Turns the electromagnet off
-// Returns true when completed
-public abstract boolean magnetOff ();
+         boolean[] boardState=new boolean[256];
+         for(int i=0;i<board.length;i++)
+             for(int j=0;j<board[i].length;j++)
+             {
+                 int squareNumber=squareNumber(i,j);
+                 boardState[squareNumber]=(board[i][j]==1)?true:false;
+             }
+         return boardState;
+     }
 
-// Returns current status of magnet
-// Could just be stored locally
-public abstract boolean getMagnetStatus ();
+     public byte playerTurn()
+     {
+         try
+         {
+             byte[] data={ENDOFCOMPUTER};
+             os.write(data);
+         }
+         catch(IOException e)
+         {
+             System.out.print("Error signal sent by computer after its turn: ");
+             System.err.println(e);
+         }
+//Notify the DE2 board that the computer has finished its turn so the player can start.
+         byte playerOperation=ERRORSIGNAL;
+         try
+         {
+             byte[] data=new byte[1];
+             while(true)
+             {
+                 playerOperation=(byte)is.read(data);
+                 if(playerOperation==RESIGN||playerOperation==ENDOFPLAYER) break;
+//Any other signals received will be discarded apart from RESIGN and ENDOFPLAYER.
+             }
+         }
+         catch(Exception e)
+         {
+             System.out.print("Acknowledgement receiving error has occured: ");
+             System.err.println(e);
+         }
+         return playerOperation;
+     }
 
-// Resets the read head to a known square and updates local storage of this value
-// Turns off electromagnet if on
-// Uses touch switches installed to detect when head is reaching extremities of motion
-// Returns true when done
-public abstract boolean resetHead ();
+     private byte squareNumber(int x, int y)
+     {
+         byte s;
+         if(x<9&&x>0) s=(byte)(8-x);
+         else s=(x==0)?(byte)9:8;
+         s<<=4;
+         if(y<9&&y>0) s+=(byte)(y-1);
+         else s+=(y==0)?(byte)8:9;
+         return s;
+     }
 
-// Returns current square of magnet head
-// Could just store value locally
-// See Requirements Specification for integer values
-public abstract int getSquare ();
+     private void transmit(byte signal, String method)
+     {
+         byte[] data=new byte[1];
+         data[0]=signal;
+         try
+         {
+             os.write(data);
+         }
+         catch(Exception e)
+         {
+             System.out.print("Error at "+method+" : ");
+             System.err.println(e);
+         }
+     }
 
-// Returns a 100-element array of sensor output
-// If reed switches are significantly offset from magnet then will require this to be done on the hardware side, as moveHead movements will not center the sensors on the squares.
-// Otherwise could just create helper methods to read off a rpackage projectBravo;ow of sensor outputs, and use resetHead / moveHead to control movement
-// Array should contain true for presence of a piece or false otherwise
-// Array key should correspond to integer representation of squares - see requirements specification
-public abstract boolean[] sensorOutput ();
-
+     private byte[] receive(int byteNumber, String method,byte[] validSignal)
+     {
+         byte[] data=new byte[byteNumber];
+         int isSignalOK=-1;
+         try
+         {
+             Outter:while(true)
+             {
+                 isSignalOK=is.read(data);
+                 if(isSignalOK<0) return null;
+                 for(int i=0;i<validSignal.length;i++)
+                     if(data[0]==validSignal[i]) break Outter;
+//Any other signals received will be discarded apart from RESIGN and ENDOFPLAYER.
+             }
+         }
+         catch(Exception e)
+         {
+             System.out.print("Error at "+method+" : ");
+             System.err.println(e);
+         }
+         return null;
+     }
+     private boolean validityCheck(int[][] board)
+     {
+         int pieceNumber=0;
+         for(int i=0;i<board.length;i++)
+             for(int j=0;j<board[i].length;j++)
+             {
+                 if(i>0&&i<9&&j>0&&j<9&&(i+j)%2==0&&board[i][j]!=0) return false;
+                 if(board[i][j]!=0) pieceNumber++;
+             }
+         if(pieceNumber!=36) return false;
+         return true;
+     }
 }
+
