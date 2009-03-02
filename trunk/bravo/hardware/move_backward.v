b6a255cf4cacd5d5f438dc0bcd623ce97f9a2fd9
infinity0x <infinity0x@e717ac08-e7cb-11dd-b882-2156224b14b4>
2009-03-02 10:22:10 +0000

    update verilog code

--- trunk/bravo/hardware/move_backward.v
+++ trunk/bravo/hardware/move_backward.v
@@ -1,55 +1,61 @@
 
-// This module is exactly the same as moveForward except
-// that the states in the case statement below have been
-// reversed (so they will drive the motor in the opposite
-// direction).
+/*
+This module is exactly the same as move_forward except that the states
+in the case statement below have been reversed (so they will drive the
+motor in the opposite direction). See move_forward for details on how
+the module works.
+*/
 
 `include "defines.v"
 
-module moveBackward(
+module move_backward(
 	input clk,
 	input go,
 	input [11:0] steps,
 	input [3:0] old_state,
 	input boundary,
 	output [3:0] state,
-	output done
+	output reg done
 	);
 	
+	// state is the signal to the motor though its 4 input wires
 	reg [3:0] mystate = 4'b1100;
 	assign state = mystate;
 	
+	//counter for speed, counts the clock cycles between changing pulses
 	reg [23:0] counter = 0;
 	
-	reg [23:0] speed = `speed;
-	
+	//counter for steps done so far
 	reg [11:0] mysteps = 0;
 	
-	reg im_done = 0;
-	assign done = im_done;
-	
 	always@(posedge clk) begin
-	if (go && boundary) im_done <= 1;
-	else if (go && !im_done) begin
-		if (mysteps	< steps) begin
-			counter <= counter+1;
-			if (counter == speed) begin
-				mysteps <= mysteps+1;
-				case (mystate)
-					4'b1100: mystate <= 4'b1001;
-					4'b1001: mystate <= 4'b0011;
-					4'b0011: mystate <= 4'b0110;
-					4'b0110: mystate <= 4'b1100;
-				endcase
-				counter <= 0;
+	// on the edge, can't move any further in that direction so stop
+		if (go && boundary) done <= 1;
+		else if (go && !done) begin
+			if (mysteps	< steps) begin
+			// count up clock cycles since last step
+				counter <= counter+1;
+				if (counter == `speed) begin
+					mysteps <= mysteps+1;
+					// change state to move a step and increase step counter
+					case (mystate)
+						4'b1100: mystate <= 4'b1001;
+						4'b1001: mystate <= 4'b0011;
+						4'b0011: mystate <= 4'b0110;
+						4'b0110: mystate <= 4'b1100;
+					endcase
+					// reset counter because a step was taken
+					counter <= 0;					
+				end
 			end
+			// number of steps reached
+			else done <= 1;
 		end
-		else im_done <= 1;
-	end
-	else if (!go) begin
-		im_done <= 0;
-		mysteps <= 0;
-		mystate <= old_state;
-	end
+		// if not moving or done reset registers
+		else if (!go) begin
+			done <= 0;
+			mysteps <= 0;
+			mystate <= old_state;
+		end
 	end
 endmodule
--- trunk/bravo/hardware/move_forward.v
+++ trunk/bravo/hardware/move_forward.v
@@ -1,37 +1,36 @@
 
-// This module is used to make a motor move forward.
-// When activated, it rotates the motor by the inputted
-// number of steps. It outputs the appropriate sequence
-// in 'state' to drive the motor in the correct direction.
-//
-// Input: activation signal and number of steps.
-//
-// Output: signals to drive motors held in 'state'. 'Done'
-// signal pulsed high when move is finished.
+/*
+This module is used to make a motor move forward. When activated, it rotates
+the motor by the inputted number of steps. It outputs the appropriate sequence
+in 'state' to drive the motor in the correct direction.
+
+Input: activation signal and number of steps.
+
+Output: signals to drive motors held in 'state'. 'Done' signal pulsed high
+when move is finished.
+*/
 
 `include "defines.v"
 
-module moveForward(
+module move_forward(
 	input clk,
 	input go,
 	input [11:0] steps,
 	input [3:0] old_state,
 	input boundary,
 	output [3:0] state,
-	output done
+	output reg done
 	);
 	
+	// state is the signal to the motor though its 4 input wires
 	reg [3:0] mystate = 4'b1100;
 	assign state = mystate;
 	
+	//counter for speed, counts the clock cycles between changing pulses
 	reg [23:0] counter = 0;
 	
-	reg [23:0] speed = `speed;
-	
+	//counter for steps done so far
 	reg [11:0] mysteps = 0;
-	
-	reg im_done = 0;
-	assign done = im_done;
 	
 	// This code first checks to see if it has been activated.
 	// It then uses 2 counters, one to control the motor speed
@@ -41,32 +40,38 @@ module moveForward(
 	// then it will update 'state' to the next signal required
 	// to drive the motors.
 	always@(posedge clk) begin
-	if (go && boundary) im_done <= 1;
-	else if (go && !im_done) begin
-		if (mysteps	< steps) begin
-			counter <= counter+1;
-			// Because the system clock is much faster than what
-			// the motor can handle, 'counter' is used to slow it
-			// down. Essentially, we have created a new clock with
-			// frequency = clk/speed
-			if (counter == speed) begin
-				mysteps <= mysteps+1;
-				case (mystate)
-					4'b1100: mystate <= 4'b0110;
-					4'b0110: mystate <= 4'b0011;
-					4'b0011: mystate <= 4'b1001;
-					4'b1001: mystate <= 4'b1100;
-				endcase
-				counter <= 0;
+	// on the edge, can't move any further in that direction so stop
+		if (go && boundary) done <= 1;
+		else if (go && !done) begin
+			if (mysteps	< steps) begin
+				counter <= counter+1;
+				// Because the system clock is much faster than what
+				// the motor can handle, 'counter' is used to slow it
+				// down. Essentially, we have created a new clock with
+				// frequency = clk/speed
+				if (counter == `speed) begin
+					mysteps <= mysteps+1;
+					case (mystate)
+						//This sequence of states drives the motor
+						//in the correct direction. This was given
+						//in the specs of the stepper motors.
+						4'b1100: mystate <= 4'b0110;
+						4'b0110: mystate <= 4'b0011;
+						4'b0011: mystate <= 4'b1001;
+						4'b1001: mystate <= 4'b1100;
+					endcase
+					// reset counter because a step was taken
+					counter <= 0;
+				end
 			end
+			// number of steps reached
+			else done <= 1;
 		end
-		else im_done <= 1;
-	end
-	// Reset registers once module is deactivated.
-	else if (!go) begin
-		im_done <= 0;
-		mysteps <= 0;
-		mystate <= old_state;
-	end
+		// if not moving or done reset registers
+		else if (!go) begin
+			done <= 0;
+			mysteps <= 0;
+			mystate <= old_state;
+		end
 	end
 endmodule
